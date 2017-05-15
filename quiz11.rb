9a563638da636a0102804cb801bd681d9f467917
MelissaKroese <mel@ixperience.co.za>
2017-05-15 21:09:25 +0200

    add quiz11

---
+++ quiz11.rb
@@ -0,0 +1,19 @@
+counts = Hash.new(0)
+
+['1', '3', '5', '3', '7', '3', '1', '1', '5'].each do |x|
+  counts[x] = counts[x] + 1
+end
+
+# counts['1'] = 1
+# counts: {'1' => 1}
+
+# counts['3'] = 1
+# counts: {'1' => 1, '3' => 1}
+
+# counts['5'] = 1
+# counts: {'1' => 1, '3' => 1, '5' => 1}
+
+# counts['3'] = 2
+# counts: {'1' => 1, '3' => 2, '5' => 1}
+
+puts counts
