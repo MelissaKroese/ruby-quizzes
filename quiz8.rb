887aea241d51f532cb7cb693e5ae289b00cd0f9f
MelissaKroese <mel@ixperience.co.za>
2017-07-01 19:13:37 +0200

    returning all values

--- quiz8.rb
+++ quiz8.rb
@@ -1,7 +1,12 @@
 def odd_values(values)
+  # create an empty array
+  result = []
   values.each_with_index do |value, index|
     if (index % 2 == 0)
-      puts "
-
+    result.insert(value)
     end
-    
+end
+end
+
+puts odd_values([5, 6, 8, 9, 3]) # => [5, 8, 3]
+puts odd_values([1, 5, 6, 8, 9, 3]) # => [1, 6, 9]
